4dcd5cf9d8f2ba0f71e70977ad2226a2a3ddc358
jackn <neelemanjack@gmail.com>
2022-07-27 21:34:32 -0700

    Removed debug command and fixed async issue

--- main.py
+++ main.py
@@ -87,7 +87,6 @@ async def skip(ctx):
 
 @data.bot.slash_command(guild_ids=data.local_config["guilds"], description="Shows the current song queue")
 async def queue(ctx):
-    await vc_insult_nj()
     embed = discord.Embed(
         title="Current Song Queue",
         description="",
@@ -150,7 +149,7 @@ async def vc_insult_nj():
     if vc.is_playing():
         vc.stop()
     if vc.channel.id != data.general.id:
-        vc.disconnect()
+        await vc.disconnect()
         data.general.connect()
     file = basic_functions.random_file(data.config["audio_files_dir"])
     vc.play(discord.FFmpegPCMAudio(file), after=lambda err: data.bot.loop.create_task(skip_song()))
